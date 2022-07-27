170fb677a1208a19fd794e0f20719d12718facba
王龙 <18392700041@163.com>
2022-07-27 11:49:19 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,10 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a
+      href="https://vitejs.dev"
+      target="_blank"
+    >
       <img
         src="/vite.svg"
         class="logo"
